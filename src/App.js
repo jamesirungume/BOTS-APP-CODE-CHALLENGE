c57f2d13e44f3bd93a52b0c4c59903db9cd490e5
Your Name <jymohcooler@gmail.com>
2023-07-29 01:22:16 +0300

    displayed the bots profiles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [Bots, setBots] = useState([]);
+  comst []
+
+  function handleBotAdd() {
+  
+   }
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then(resp => resp.json())
+      .then(data => {
+        setBots(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '60px' }}>
+      {Bots.map(bot => (
+        <div key={bot.id} className="bot-card" onClick={handleBotAdd}>
+          <img src={bot.avatar_url} alt={bot.name} />
+          <li>{bot.name}</li>
+          <p>Health: {bot.health}</p>
+          <p>Damage: {bot.damage}</p>
+          <p>Armor: {bot.armor}</p>
+          <p>Class: {bot.bot_class}</p>
+          <div className="catchphrase-data">{bot.catchphrase}</div>
+
+        </div>
+      ))}
     </div>
   );
 }
