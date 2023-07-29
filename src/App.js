1b16763e790a967188e652f0cdb3c2f6a3d610b3
Your Name <jymohcooler@gmail.com>
2023-07-29 05:18:18 +0300

    added an advanced deliverable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import YourBotArmy from './YourBotArmy';
 import BotDetails from './BotDetails';
+import BotsCollection from './BotsCollection';
 
 function App() {
   const [Bots, setBots] = useState([]);
-  const [addBot,setAddBot] = useState([])
-  const [viewSpecs,setViewSpecs] = useState(null)
+  const [addBot, setAddBot] = useState([]);
+  const [viewSpecs, setViewSpecs] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3000/bots")
@@ -21,57 +21,46 @@ function App() {
   }, []);
 
   function handleGoBack() {
-   setViewSpecs(null)
+    setViewSpecs(null);
   }
-   
 
   function handleViewDetails(bot) {
-    setViewSpecs(bot)
+    setViewSpecs(bot);
   }
 
   function handleBotAdd(bot) {
-     const findBot = addBot.find(Bot => Bot.id === bot.id)
-     if(!findBot) {
-      setAddBot([...addBot,bot])
-      setViewSpecs(null); 
-     }
-   }
-  
-   function handleRealese(bot) {
-      setAddBot((addBot) =>  addBot.filter(armybot => armybot.id !== bot.id ))
-   }
-   function handleDelete(bot) {
-  
-    fetch (`http://localhost:3000/bots/${bot.id}`, {
+    const findBot = addBot.find(Bot => Bot.id === bot.id);
+    if (!findBot) {
+      setAddBot([...addBot, bot]);
+      setViewSpecs(null);
+    setBots((Bots) => Bots.filter(Bot => Bot.id !== bot.id))
+    }
+  }
+
+  function handleRealese(bot) {
+    setAddBot((addBot) => addBot.filter(armybot => armybot.id !== bot.id));
+  }
+
+  function handleDelete(bot) {
+    fetch(`http://localhost:3000/bots/${bot.id}`, {
       method: "DELETE"
     })
     .then(data => {
-      setAddBot((addBot) =>  addBot.filter(armybot => armybot.id !== bot.id ))
-    })
-   }
+      setAddBot((addBot) => addBot.filter(armybot => armybot.id !== bot.id));
+    });
+  }
 
   return (
-  <div>
-    {viewSpecs ? 
-     (<BotDetails viewSpecs={viewSpecs} onEnlistBot={handleBotAdd} onGoBackToList={handleGoBack}/>):
-    (<YourBotArmy addBot={addBot} onRealese={handleRealese} onDelete={handleDelete}/>)  
-}
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '52px' }}>
-      {Bots.map(bot => (
-        <div key={bot.id} className="bot-card" onClick={() => handleViewDetails(bot)}>
-          <img src={bot.avatar_url} alt={bot.name} />
-          <li>{bot.name}</li>
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <p>Armor: {bot.armor}</p>
-          <p>Class: {bot.bot_class}</p>
-          <div className="catchphrase-data">{bot.catchphrase}</div>
-
-        </div>
-      ))}
-   </div>
-      
-  </div>
+    <div>
+      {viewSpecs ? (
+        <BotDetails viewSpecs={viewSpecs} onEnlistBot={handleBotAdd} onGoBackToList={handleGoBack} />
+      ) : (
+        <>
+          <YourBotArmy addBot={addBot} onRealese={handleRealese} onDelete={handleDelete} />
+          <BotsCollection Bots={Bots} addBot={addBot} onViewDetails={handleViewDetails} />
+        </>
+      )}
+    </div>
   );
 }
 
