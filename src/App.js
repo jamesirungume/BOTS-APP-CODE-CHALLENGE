86d07e94663f61d92f8dff35e23b2bf9235b677b
Your Name <jymohcooler@gmail.com>
2023-07-30 12:46:02 +0300

    added a filter by class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import YourBotArmy from './YourBotArmy';
-import BotDetails from './BotDetails';
-import BotsCollection from './BotsCollection';
-import FilterByClass from './FilterByClass';
-
+import React, { useState, useEffect } from "react";
+import YourBotArmy from "./YourBotArmy";
+import BotDetails from "./BotDetails";
+import BotsCollection from "./BotsCollection";
+import FilterByClass from "./FilterByClass";
 
 function App() {
   const [Bots, setBots] = useState([]);
   const [addBot, setAddBot] = useState([]);
   const [viewSpecs, setViewSpecs] = useState(null);
+  const [selectedmyClass, setSelectedmyClasss] = useState([]);
+  const [filteredBots, setFilteredBots] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/bots")
-      .then(resp => resp.json())
-      .then(data => {
+      .then((resp) => resp.json())
+      .then((data) => {
         setBots(data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
+
+  useEffect(() => {
+    setFilteredBots(
+      Bots.filter((bot) => {
+        if (selectedmyClass.length === 0) return true;
+        return selectedmyClass.includes(bot.bot_class);
+      })
+    );
+  }, [selectedmyClass, Bots]);
+
+  function handlefilterClass(classe) {
+    setSelectedmyClasss(classe);
+  }
 
   function handleGoBack() {
     setViewSpecs(null);
@@ -31,36 +44,47 @@ function App() {
   }
 
   function handleBotAdd(bot) {
-    const findBot = addBot.find(Bot => Bot.id === bot.id);
+    const findBot = addBot.find((Bot) => Bot.id === bot.id);
     if (!findBot) {
       setAddBot([...addBot, bot]);
       setViewSpecs(null);
-    setBots((Bots) => Bots.filter(Bot => Bot.id !== bot.id))
+      setBots((Bots) => Bots.filter((Bot) => Bot.id !== bot.id));
     }
   }
 
   function handleRealese(bot) {
-    setAddBot((addBot) => addBot.filter(armybot => armybot.id !== bot.id));
+    setAddBot((addBot) => addBot.filter((armybot) => armybot.id !== bot.id));
   }
 
   function handleDelete(bot) {
     fetch(`http://localhost:3000/bots/${bot.id}`, {
-      method: "DELETE"
-    })
-    .then(data => {
-      setAddBot((addBot) => addBot.filter(armybot => armybot.id !== bot.id));
+      method: "DELETE",
+    }).then((data) => {
+      setAddBot((addBot) => addBot.filter((armybot) => armybot.id !== bot.id));
     });
   }
 
   return (
     <div>
       {viewSpecs ? (
-        <BotDetails viewSpecs={viewSpecs} onEnlistBot={handleBotAdd} onGoBackToList={handleGoBack} />
+        <BotDetails
+          viewSpecs={viewSpecs}
+          onEnlistBot={handleBotAdd}
+          onGoBackToList={handleGoBack}
+        />
       ) : (
-        <> 
-          <FilterByClass />
-          <YourBotArmy addBot={addBot} onRealese={handleRealese} onDelete={handleDelete} />
-          <BotsCollection Bots={Bots} addBot={addBot} onViewDetails={handleViewDetails} />
+        <>
+          <FilterByClass onClassChange={handlefilterClass} />
+          <YourBotArmy
+            addBot={addBot}
+            onRealese={handleRealese}
+            onDelete={handleDelete}
+          />
+          <BotsCollection
+            Bots={filteredBots}
+            addBot={addBot}
+            onViewDetails={handleViewDetails}
+          />
         </>
       )}
     </div>
--- src/FilterByClass.js
+++ src/FilterByClass.js
@@ -1,28 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function FilterByClass()  {
-    const [selectClasse,setSelectedClassse] = useState([])
-    const classes = ["Support", "Medic", "Assault", "Defender", "Captain", "Witch"]
-  
-function handleClassChange(e) {
-const 
-}
+function FilterByClass({ onClassChange }) {
+  const [selectClasse, setSelectedClassse] = useState([]);
+  const classes = ["Support", "Medic", "Assault", "Defender", "Captain", "Witch"];
 
-    return (
- <div>
-    {classes.map(classe => (
-     <label>
-        <input
-        type="checkbox"
-        value={classe}
-        checked={selectClasse.includes(classe)}
-        onChange={handleClassChange}
-        />
-     </label>
-            ))}
-         
-  </div>
-    )
+  useEffect(() => {
+    onClassChange(selectClasse);
+  }, [selectClasse]);
+
+  function handleClassChange(e) {
+    const selectedClass = e.target.value;
+
+    if (e.target.checked) {
+      setSelectedClassse((prevClass) => [...prevClass, selectedClass]);
+    } else {
+      setSelectedClassse((prevClass) =>
+        prevClass.filter((classe) => classe !== selectedClass)
+      );
+    }
+  }
+
+  return (
+    <div>
+      {classes.map((classe) => (
+        <label key={classe}>
+          <input
+            type="checkbox"
+            value={classe}
+            checked={selectClasse.includes(classe)}
+            onChange={handleClassChange}
+          />
+          {classe}
+        </label>
+      ))}
+    </div>
+  );
 }
 
 export default FilterByClass;
